e4ab6241c3ce837878fa1d40cb1b6289d84f2a38
Joshua Maurice Yaacoub <103109827+juswaa101@users.noreply.github.com>
2023-03-17 14:55:08 +0800

    Added Favicon
    added favicon to website

--- resources/views/components/scripts.blade.php
+++ resources/views/components/scripts.blade.php
@@ -34,6 +34,8 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="{{ asset('css/stepper.css') }}">
 
+<link rel="icon" type="image/x-icon" href="{{ asset('images/global_icon.png') }}"></link>
+
 {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> --}}
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css" />
 <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
